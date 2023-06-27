c183299bd210258fb23062af2474426404565a34
Oscar <chikenlitle99@hotmail.com>
2023-06-27 00:33:11 -0600

    Ready for changue Language

---
+++ www/languages/lang.en.js
@@ -0,0 +1,28 @@
+lang = {
+  greeting: 'Hello',
+};
+
+// document.onload = function() {
+//     alert(lang.greeting);
+// };
+
+// {
+//     "en": {
+//         "Hello": "Hello World!",
+//         "SaveChanges": "Do you want to save the changes?",
+//         "Yes": "Yes",
+//         "No": "No"
+//     },
+//     "fr": {
+//         "Hello": "Bonjour à tous!",
+//         "SaveChanges": "Voulez-vous sauvegarder les modifications?",
+//         "Yes": "Oui",
+//         "No": "Non"
+//     },
+//     "zh": {
+//         "Hello": "大家好",
+//         "SaveChanges": "是否要保存更改？",
+//         "Yes": "要",
+//         "No": "不要"
+//     }
+// }
